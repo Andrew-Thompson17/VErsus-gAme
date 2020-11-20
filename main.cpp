024af83f3263c68415c40e475a7077172c62b715
Andrew-Thompson17 <at781419@ohio.edu>
2020-11-20 16:16:48 -0500

    main file from repl.it

---
+++ main.cpp
@@ -0,0 +1,26 @@
+/**
+ *   @file: VErsus-gAme.cpp
+ * @author: Andrew Thompson & Vishnu & Eric
+ *   @date: 11/13/20
+ *  @brief: 
+ */
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+#include <vector>
+#include "game.h"
+#include "player.h"
+
+using namespace std;
+
+int main(){
+  srand(time(NULL));
+  string personName;
+
+  cout << "Enter your name: ";
+  cin >> personName;
+  Player newPlayer(personName);
+  Game newGame(newPlayer);
+                
+  return 0;
+}
